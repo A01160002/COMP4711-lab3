db52e07cebaeab33734961cfba7fec541ad988db
33950 <33950@DESKTOP-CM1MCPP>
2020-09-22 11:25:51 +0800

    add my self data

--- index.php
+++ index.php
@@ -13,26 +13,43 @@ and open the template in the editor.
         <?php
             include('Student.php'); 
             $students = array(); 
+            
+            $first = new Student(); 
+            $first->surname = "Doe"; 
+            $first->first_name = "John"; 
+            $first->add_email('home','john@doe.com'); 
+            $first->add_email('work','jdoe@mcdonalds.com'); 
+            $first->add_grade(65); 
+            $first->add_grade(75); 
+            $first->add_grade(55); 
+            $students['j123'] = $first; 
+
+            $second = new Student(); $second->surname = "Einstein"; 
+            $second->first_name = "Albert"; 
+            $second->add_email('home','albert@braniacs.com'); 
+            $second->add_email('work1','a_einstein@bcit.ca'); 
+            $second->add_email('work2','albert@physics.mit.edu'); 
+            $second->add_grade(95); 
+            $second->add_grade(80); 
+            $second->add_grade(50); 
+            $students['a456'] = $second;
+            
+            $third = new Student(); $third->surname = "Liu"; 
+            $third->first_name = "Chang"; 
+            $third->add_email('home','3395097772@qq.com'); 
+            $third->add_email('work1','liuchang@bcit.ca'); 
+            $third->add_email('work2','liuchang@physics.mit.edu'); 
+            $third->add_grade(60); 
+            $third->add_grade(60); 
+            $third->add_grade(60); 
+            $students['A01160002'] = $third;
+            
+            ksort($students); // one of the many sort functions
+            
+            
             foreach($students as $student) 
                 echo $student->toString(); 
         ?>
     </body>
 </html>
 
-$first = new Student(); 
-$first->surname = "Doe"; 
-$first->first_name = "John"; 
-$first->add_email('home','john@doe.com'); 
-$first->add_email('work','jdoe@mcdonalds.com'); 
-$first->add_grade(65); 
-$first->add_grade(75); 
-$first->add_grade(55); 
-$students['j123'] = $first; 
-
-$second = new Student(); $second->surname = "Einstein"; 
-$second->first_name = "Albert"; 
-$second->add_email('home','albert@braniacs.com'); 
-$second->add_email('work1','a_einstein@bcit.ca'); 
-$second->add_email('work2','albert@physics.mit.edu'); 
-$second->add_grade(95); $second->add_grade(80); 
-$second->add_grade(50); $students['a456'] = $second;
